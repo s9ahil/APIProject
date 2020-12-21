7e5f492b6e992170562dcef1be54a2c85e68ef77
Sahil9591 <Sahil9591@gmail.com>
2020-12-21 09:36:18 +0530

    commit

--- APIAuto/src/test/java/org/testing/runner/RunnerClass.java
+++ APIAuto/src/test/java/org/testing/runner/RunnerClass.java
@@ -2,15 +2,19 @@ package org.testing.runner;
 
 import java.io.IOException;
 
+import javax.xml.parsers.ParserConfigurationException;
+
 import org.testing.TestScripts.DeleteRequestTC;
 import org.testing.TestScripts.GetRequestTC;
 import org.testing.TestScripts.GetRequestWithQueryparameter;
 import org.testing.TestScripts.PostRequestTC;
 import org.testing.TestScripts.PutRequestTC4;
+import org.testing.utilities.XMLParsing;
+import org.xml.sax.SAXException;
 
 public class RunnerClass {
 
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args) throws IOException, ParserConfigurationException, SAXException {
 	/*	 PostRequestTC post = new PostRequestTC();
 		 post.tc1();
 	     
@@ -26,6 +30,9 @@ public class RunnerClass {
 		 DeleteRequestTC del =new DeleteRequestTC();
 		 del.tc5(); */
 		 
+		XMLParsing x =new XMLParsing();
+		x.XMLhandling("C:\\Users\\sahil\\eclipse-workspace\\XML\\responsedata.xml","title");
+		
 	}
 }
  
